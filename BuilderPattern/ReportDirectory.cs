7677d0b6f2a4eb527953e878d1ecc1cdac1cc430
Aakriti Kumari <kumariaakriti1404@gmail.com>
2023-09-04 17:03:20 +0530

    Builder Pattern

---
+++ BuilderPattern/ReportDirectory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPattern
+{
+    public class ReportDirectory
+    {
+        public Report MakeReport(IReportBuilder reportBuilder)
+        {
+            reportBuilder.CreateNewReport();
+            reportBuilder.SetReportType();
+            reportBuilder.SetReportHeader();
+            reportBuilder.SetReportContent();
+            reportBuilder.SetReportFooter();
+
+            return reportBuilder.GetReport();
+            
+        }
+    }
+}
