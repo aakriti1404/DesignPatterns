e8395f2fba46ed618927a253afc523cceaa96189
Aakriti Kumari <kumariaakriti1404@gmail.com>
2023-09-06 21:15:25 +0530

    Proxy Pattern
    It contains an example of a secured folder which can only be accessed by CEO and Manager. And if some employee having role as Developer try to access this file , a message will be shown that "Sorry! You don't have permission to access Secured Folder.". And to make this whole system work there is a FolderProxy which decide which folder to call General Folder or Secured Folder based on the Role of employee.

--- ProxyPattern/Program.cs
+++ ProxyPattern/Program.cs
@@ -9,14 +9,14 @@ namespace ProxyPattern
         {
             Console.WriteLine("Client passing employee with Role Developer to folderproxy");
             Employee employee1 = new Employee("Anurag", "Anurag123", "Developer");
-            ProxyFolder folderProxy1 = new ProxyFolder(employee1);
+            FolderProxy folderProxy1 = new FolderProxy(employee1);
             folderProxy1.PerformOperation();
 
             Console.WriteLine();
 
             Console.WriteLine("Client passing employee with Role Manager to folderproxy");
             Employee employee2 = new Employee("Pranaya", "Pranaya123", "Manager");
-            ProxyFolder folderProxy2 = new ProxyFolder(employee2);
+            FolderProxy folderProxy2 = new FolderProxy(employee2);
             folderProxy2.PerformOperation();
 
             Console.Read();
