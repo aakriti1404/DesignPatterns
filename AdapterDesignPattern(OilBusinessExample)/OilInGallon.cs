73364a753fd60467ac43261db9db36432d0bdd1d
Aakriti Kumari <kumariaakriti1404@gmail.com>
2023-09-06 07:34:07 +0530

    Adapter Pattern
    It contain example of Oil Business where the buyer uses the system which can measure the quantity of oil in gallon but the seller have system which measure the quantity of oil in liter. So a adapter is used in between which  can convert the quantity of oil in gallon to liter. And then the final cost is calculated in dollars.

---
+++ AdapterDesignPattern(OilBusinessExample)/OilInGallon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterDesignPattern_OilBusinessExample_
+{
+    public class OilInGallon
+    {
+        private double oilPrice;
+        public OilInGallon(double quantityInGallon)
+        { 
+            this.oilPrice = quantityInGallon * 2.98; //price is 2.98 dollars per liter
+            Console.WriteLine("Total cost of purchase is " + oilPrice + " dollars.");
+            Console.WriteLine("Purchase Complete");
+        }
+    }
+}
