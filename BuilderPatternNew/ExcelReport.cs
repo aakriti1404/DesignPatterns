db1f11b26aa2927d6a148a02a8fbc9b56c5a8e99
Aakriti Kumari <kumariaakriti1404@gmail.com>
2023-09-06 07:23:21 +0530

    Builder Pattern (New)
    This is the new code

---
+++ BuilderPatternNew/ExcelReport.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPatternNew
+{
+    public class ExcelReport : IReportBuilder
+    {
+        private Report reportObject=new Report();
+
+        public void SetReportType(string reportType)
+        {
+            reportObject.ReportType = reportType;
+        }
+        public void SetReportHeader(string reportHeader)
+        {
+            reportObject.ReportHeader = reportHeader;
+        }
+        public void SetReportContent(string reportContent)
+        {
+            reportObject.ReportContent = reportContent;
+        }
+        public void SetReportFooter(string reportFooter)
+        {
+            reportObject.ReportFooter = reportFooter;
+        }
+        public IReport GetReport()
+        {
+            return reportObject;
+        }
+    }
+}
---
+++ BuilderPatternNew/IReportBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPatternNew
+{
+    public interface IReportBuilder
+    {
+        void SetReportType(string reportType);
+        void SetReportHeader(string reportHeader);
+        void SetReportContent(string reportContent);
+        void SetReportFooter(string reportFooter);
+        IReport GetReport();
+
+
+    }
+}
